937242a9a1cb780d5852226f269b7ff331f8f174
Dean DeHart <DeHartDean@gmail.com>
2022-02-20 13:21:02 -0500

    Add stuff

--- blog/app/screens/api/blogAPI.ts
+++ blog/app/screens/api/blogAPI.ts
@@ -1,5 +1,26 @@
+interface NewPostPayload {
+  title: string;
+  body: string;
+}
+
 export async function getPosts() {
   const response = await fetch("http://localhost:3000/api/v1/posts");
   const posts = await response.json();
   return posts;
+}
+
+export async function createPost(payload: NewPostPayload) {
+  const requestInfo = {
+    method: "POST",
+    body: JSON.stringify(payload),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  const response = await fetch(
+    "http://localhost:3000/api/v1/posts",
+    requestInfo
+  );
+  const createdPost = await response.json();
+  return createdPost;
 }
